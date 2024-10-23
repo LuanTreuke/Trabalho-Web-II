784b647162a30bdb661aeb61ff150b4df0728525
LuanTreuke <128757273+LuanTreuke@users.noreply.github.com>
2024-10-23 19:34:06 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,99 @@
+<?php include('config.php') ?>
+
+<!DOCTYPE html>
+<html lang="en, pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Descrição do site">
+    <meta name="keywords" content="Palavra chave 1, 2, 3, etc...">
+
+    <link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>assets/css/style.css">
+    <!-- Fonte -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
+    <!-- Icone -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Projeto 1</title>
+</head>
+
+<body>
+    
+    <?php 
+    // <!-- Obtendo a irl se existir ou home. isset=se existe, ?=então, :=senão
+    $url = isset($_GET['url']) ? $_GET['url'] : 'home';
+    switch($url) {
+        case 'depoimentos': 
+            echo '<target="depoimentos"/>';
+            break;
+        case 'servicos':
+            echo '<target="servicos"/>';
+            break;
+    }
+    
+    ?>
+    
+
+    <header>
+        <div class="center">
+            <div class="logo left">Logomarca</div>
+            <nav class="desktop right">
+                <ul>
+                    <li><a href="<?php echo INCLUDE_PATH;?>">Home</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH;?>depoimentos">Depoimentos</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH;?>servicos">Serviços</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH;?>contato">Contato</a></li>
+                </ul>
+            </nav>
+            <nav class="mobile right">
+                <div class="bars-mobile fa-solid fa-bars"></div>
+                <ul>
+                    <li><a href="<?php echo INCLUDE_PATH;?>">Home</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH;?>depoimentos">Depoimentos</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH;?>servicos">Serviços</a></li>
+                    <li><a href="<?php echo INCLUDE_PATH;?>contato">Contato</a></li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </div>
+    </header>
+
+    <?php 
+        //Se url existe
+        if(file_exists('pages/'.$url.'.php')){
+            include('pages/'.$url.'.php');
+        }else{
+            if($url != 'depoimentos' && $url != 'servicos'){
+                $pagina404 = true;
+                include('pages/404.php');
+            }else{
+               include('pages/home.php');
+            }
+
+            $pagina404 = true;
+            include('pages/404.php');
+        }
+    ?>
+
+    <footer <?php if(isset($pagina404) && $pagina404 == true) echo 'class = "fixed"'; ?>>
+        <div class="center">
+            <p>Todos os direitos reservados!</p>
+        </div>
+    </footer>
+
+    <!-- jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"
+        integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="<?php echo INCLUDE_PATH;?>assets/js/scripts.js"></script>    
+
+</body>
+
+</html>
---
+++ pages/home.php
@@ -0,0 +1,147 @@
+<section class="banner-principal">
+        <div class="overlay"></div>
+        <div class="center">
+            <form action="">
+                <h2>Qual o seu melhor e-mail?</h2>
+                <input type="email" name="email" id="email" required>
+                <input type="submit" name="enviar" value="Enviar">
+            </form>
+        </div>
+    </section>
+
+    <section class="descricao-autor">
+        <div class="center">
+            <div class="w50 left">
+                <h2>Luan Treuke</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor neque quis lacus porttitor,
+                    non molestie mi faucibus. Suspendisse sit amet facilisis sapien. Nam dignissim nunc ut tortor semper
+                    consequat. Proin scelerisque imperdiet nisl, venenatis faucibus purus. Nam ac ultrices tortor, eget
+                    lobortis felis. Aenean nec dui blandit, interdum mi nec, vulputate lorem. Sed tempor id ante et
+                    maximus. Proin gravida elit at lorem laoreet tempor et sit amet ante. Phasellus id nunc a tellus
+                    vehicula consectetur sed id augue. Nunc dictum libero tellus, id tincidunt ante sodales sit amet.
+                    Cras orci lorem, ultricies sodales cursus nec, placerat quis ex. Quisque molestie ornare est sit
+                    amet varius. Aenean diam tortor, pretium quis nulla a, eleifend maximus urna. Suspendisse sed felis
+                    massa.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor neque quis lacus porttitor,
+                    non molestie mi faucibus. Suspendisse sit amet facilisis sapien. Nam dignissim nunc ut tortor semper
+                    consequat. Proin scelerisque imperdiet nisl, venenatis faucibus purus. Nam ac ultrices tortor, eget
+                    lobortis felis. Aenean nec dui blandit, interdum mi nec, vulputate lorem. Sed tempor id ante et
+                    maximus. Proin gravida elit at lorem laoreet tempor et sit amet ante. Phasellus id nunc a tellus
+                    vehicula consectetur sed id augue. Nunc dictum libero tellus, id tincidunt ante sodales sit amet.
+                    Cras orci lorem, ultricies sodales cursus nec, placerat quis ex. Quisque molestie ornare est sit
+                    amet varius. Aenean diam tortor, pretium quis nulla a, eleifend maximus urna. Suspendisse sed felis
+                    massa.</p>
+            </div>
+            <div class="w50 left">
+                <img src="<?php echo INCLUDE_PATH;?>assets\img\campus ifpr.jpg" alt="Local de trabalho">
+            </div>
+            <div class="clear"></div>
+            <!--clear float-->
+        </div>
+    </section>
+
+    <section class="especialidades">
+        <div class="center">
+            <h2 class="title">Especialidades</h2>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-html5"></i></h3>
+                <h3>HTML 5</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor neque quis lacus porttitor,
+                    non molestie mi faucibus. Suspendisse sit amet facilisis sapien. Nam dignissim nunc ut tortor semper
+                    consequat. Proin scelerisque imperdiet nisl, venenatis faucibus purus. Nam ac ultrices tortor, eget
+                    lobortis felis. Aenean nec dui blandit, interdum mi nec, vulputate lorem. Sed tempor id ante et
+                    maximus. Proin gravida elit at lorem laoreet tempor et sit amet ante. Phasellus id nunc a tellus
+                    vehicula consectetur sed id augue. Nunc dictum libero tellus, id tincidunt ante sodales sit amet.
+                    Cras orci lorem, ultricies sodales cursus nec, placerat quis ex. Quisque molestie ornare est sit
+                    amet varius. Aenean diam tortor, pretium quis nulla a, eleifend maximus urna. Suspendisse sed felis
+                    massa.</p>
+            </div>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-css3"></i></h3>
+                <h3>CSS 3</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor neque quis lacus porttitor,
+                    non molestie mi faucibus. Suspendisse sit amet facilisis sapien. Nam dignissim nunc ut tortor semper
+                    consequat. Proin scelerisque imperdiet nisl, venenatis faucibus purus. Nam ac ultrices tortor, eget
+                    lobortis felis. Aenean nec dui blandit, interdum mi nec, vulputate lorem. Sed tempor id ante et
+                    maximus. Proin gravida elit at lorem laoreet tempor et sit amet ante. Phasellus id nunc a tellus
+                    vehicula consectetur sed id augue. Nunc dictum libero tellus, id tincidunt ante sodales sit amet.
+                    Cras orci lorem, ultricies sodales cursus nec, placerat quis ex. Quisque molestie ornare est sit
+                    amet varius. Aenean diam tortor, pretium quis nulla a, eleifend maximus urna. Suspendisse sed felis
+                    massa.</p>
+            </div>
+            <div class="w33 left box-especialidades">
+                <h3><i class="fa-brands fa-js"></i></h3>
+                <h3>JS</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec porttitor neque quis lacus porttitor,
+                    non molestie mi faucibus. Suspendisse sit amet facilisis sapien. Nam dignissim nunc ut tortor semper
+                    consequat. Proin scelerisque imperdiet nisl, venenatis faucibus purus. Nam ac ultrices tortor, eget
+                    lobortis felis. Aenean nec dui blandit, interdum mi nec, vulputate lorem. Sed tempor id ante et
+                    maximus. Proin gravida elit at lorem laoreet tempor et sit amet ante. Phasellus id nunc a tellus
+                    vehicula consectetur sed id augue. Nunc dictum libero tellus, id tincidunt ante sodales sit amet.
+                    Cras orci lorem, ultricies sodales cursus nec, placerat quis ex. Quisque molestie ornare est sit
+                    amet varius. Aenean diam tortor, pretium quis nulla a, eleifend maximus urna. Suspendisse sed felis
+                    massa.</p>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </section>
+
+    <section class="extras">
+        <div class="center">
+            <div id="depoimentos" class="w50 left">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sint soluta id dignissimos
+                        dicta, in vero facere velit impedit consectetur beatae itaque totam rerum provident quibusdam
+                        officiis hic explicabo? Quidem.
+                    </p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sint soluta id dignissimos
+                        dicta, in vero facere velit impedit consectetur beatae itaque totam rerum provident quibusdam
+                        officiis hic explicabo? Quidem.
+                    </p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-descricao">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur sint soluta id dignissimos
+                        dicta, in vero facere velit impedit consectetur beatae itaque totam rerum provident quibusdam
+                        officiis hic explicabo? Quidem.
+                    </p>
+                    <p class="nome-autor">Lorem Ipsum</p>
+                </div>
+            </div>
+            <div id="servicos" class="w50 left">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum numquam odio dolore aut
+                            nesciunt quas placeat esse quae aperiam odit officia, ipsam, iusto natus reprehenderit
+                            laboriosam eveniet, fuga eum assumenda?
+                        </li>
+                        <li>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium, dolorem
+                            exercitationem. Aliquam nisi cumque ratione reiciendis, tenetur odit sed sapiente magni
+                            facilis quibusdam soluta? Consequuntur dignissimos fugiat maiores animi sunt.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam iure repellendus, ab nulla
+                            adipisci consequatur quidem temporibus at aut vero ex hic. Porro, quod consectetur quibusdam
+                            doloremque illo optio dolorum.
+                        </li>
+                        <li>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos expedita accusamus
+                            reiciendis adipisci quam placeat ea et autem, assumenda nisi aperiam quidem rerum
+                            voluptatibus tempora cum maxime dolores aliquid voluptates.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </section>
